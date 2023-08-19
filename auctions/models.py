b24813a9b954ca1a9dfb2c7226f25f7467175806
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-08-19 22:19:21 +0300

    urls and templates

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,29 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+# auction listings
+class Listings(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    active = models.BooleanField(default=True) 
+    category = models.CharField(max_length=50)
+    image = models.TextField(blank=True, null=True)
+
+# bids
+class Bids(models.Model):
+    bid = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="listing_bid")
+
+
+# comments
+class Comments(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="listing_comment")
--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("categories", views.categories, name="categories"),
+    path("categories/<str:category>", views.category, name="category"),
+    path("create", views.create, name="create"),
+    path("listing/<int:listing_id>", views.listing, name="listing"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
