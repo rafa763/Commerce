73825074803766d76603a04e01d4fb7912a8db92
rafa763 <mohamed.raafat@eng.suez.edu.eg>
2023-08-20 12:29:15 +0300

    register models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Listings, Categories
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Listings)
+admin.site.register(Categories)
